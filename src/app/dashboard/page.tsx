c190d6c3c01a61cc0fdb6a7ebd23158c15c8b606
arifnotify <arifnotify@gmail.com>
2026-07-06 22:37:38 +0300

    first commit 2

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,52 +1,64 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { getDashboardSummary } from "@/src/services/analytics.service";
+
 import StatsCards from "@/src/components/dashboard/StatsCards";
+
 import { DashboardSummary } from "@/src/types/dashboard";
+
+import { getDashboardSummary } from "@/src/services/analytics.service";
 
 export default function DashboardPage() {
   const [summary, setSummary] =
-    useState<DashboardSummary | null>(null);
+    useState<DashboardSummary | null>(
+      null
+    );
 
   const [loading, setLoading] =
     useState(true);
 
   useEffect(() => {
-    fetchData();
+    fetchSummary();
   }, []);
 
-  const fetchData = async () => {
+  const fetchSummary = async () => {
     try {
-      const res = await getDashboardSummary();
+      const data =
+        await getDashboardSummary();
 
-      console.log("SUMMARY API:", res);
-
-      setSummary(res);
-    } catch (err) {
-      console.log(err);
+      setSummary(data);
+    } catch (error) {
+      console.log(error);
     } finally {
       setLoading(false);
     }
   };
 
   if (loading) {
-    return <div>Loading Dashboard...</div>;
+    return (
+      <div className="text-gray-500">
+        Loading Dashboard...
+      </div>
+    );
   }
 
-  if (!summary) {
-    return <div>No Data Found</div>;
-  }
+  if (!summary) return null;
 
   return (
     <div className="space-y-6">
+      <div>
+        <h1 className="text-3xl font-bold">
+          Dashboard
+        </h1>
 
-      <h1 className="text-3xl font-bold">
-        Dashboard
-      </h1>
+        <p className="text-gray-500">
+          Overview of your business
+        </p>
+      </div>
 
-      <StatsCards summary={summary} />
-
+      <StatsCards
+        summary={summary}
+      />
     </div>
   );
 }
