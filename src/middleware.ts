2b6d38b885ab7404f0baeacdbfac1e68bb81ea80
arifnotify <arifnotify@gmail.com>
2026-07-06 21:22:53 +0300

    first commit2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,10 @@
-/*import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get("token");
-
-  const isLogin =
-    req.nextUrl.pathname === "/login";
-
-  if (!token && !isLogin) {
-    return NextResponse.redirect(
-      new URL("/login", req.url)
-    );
-  }
-
-  if (token && isLogin) {
-    return NextResponse.redirect(
-      new URL("/dashboard", req.url)
-    );
-  }
-
+// middleware.ts
+export function middleware() {
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/login"],
-};*/
+  matcher: [],
+};
