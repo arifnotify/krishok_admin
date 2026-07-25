8f1f3485c7b2b2bba68c8c96dde0b9e7832eeba7
Arif <90087547+arifnotify@users.noreply.github.com>
2026-07-25 19:25:57 +0300

    Refactor banner service API endpoints

--- src/services/banner.service.ts
+++ src/services/banner.service.ts
@@ -1,61 +1,36 @@
 import api from "./api";
 
-// GET ALL
-export const getBanners =
-  async () => {
-    const res =
-      await api.get(
-        "/banners",
-      );
+// GET ALL (এখানে পরিবর্তন করতে হবে)
+export const getBanners = async () => {
+  const res = await api.get("/banners/admin/all");
 
-    return res.data;
-  };
+  return res.data;
+};
 
 // GET ONE
-export const getBannerById =
-  async (id: string) => {
-    const res =
-      await api.get(
-        `/banners/${id}`,
-      );
+export const getBannerById = async (id: string) => {
+  const res = await api.get(`/banners/${id}`);
 
-    return res.data;
-  };
+  return res.data;
+};
 
 // CREATE
-export const createBanner =
-  async (data: any) => {
-    const res =
-      await api.post(
-        "/banners",
-        data,
-      );
+export const createBanner = async (data: any) => {
+  const res = await api.post("/banners", data);
 
-    return res.data;
-  };
+  return res.data;
+};
 
 // UPDATE
-export const updateBanner =
-  async (
-    id: string,
-    data: any,
-  ) => {
-    const res =
-      await api.patch(
-        `/banners/${id}`,
-        data,
-      );
+export const updateBanner = async (id: string, data: any) => {
+  const res = await api.patch(`/banners/${id}`, data);
 
-    return res.data;
-  };
+  return res.data;
+};
 
 // DELETE
-export const deleteBanner =
-  async (id: string) => {
-    const res =
-      await api.delete(
-        `/banners/${id}`,
-      );
+export const deleteBanner = async (id: string) => {
+  const res = await api.delete(`/banners/${id}`);
 
-    return res.data;
-  };
+  return res.data;
+};
