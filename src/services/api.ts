2b00fc499e26d7bd1617b93c29e24aebeac6c0c6
Arif <90087547+arifnotify@users.noreply.github.com>
2026-09-12 11:07:30 +0300

    Change API base URL from sooqxy to matigr

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "https://api.sooqxy.com",
+  baseURL: "https://api.matigr.com",
 });
 
 api.interceptors.request.use((config) => {
